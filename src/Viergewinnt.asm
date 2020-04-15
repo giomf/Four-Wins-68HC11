c72f3603a9834b3163f1db8a302544de700847db
Guillaume Fournier-Mayer <guillaume.fm@mailbox.org>
2020-04-15 14:04:41 +0200

    Add listing in 80 Format

--- src/Viergewinnt.asm
+++ src/Viergewinnt.asm
@@ -70,14 +70,21 @@ colCmdMskM			dc.b		%00010000		; Steuerkommando fürs Display
 colCmdMskL			dc.b		%00000000		; Steuerkommando fürs Display
 adrMask				dc.b		%00000111		; Maske
 
-LCDCols 			equ			128				; Anzahl an horizontalen Pixeln des Displays
-LCDRows 			equ			8				; Anzahl an vertikalen Bytes des Displays
+LCDCols 			equ			128				; Horizontale Pixeln des LCD's
+LCDRows 			equ			8				; Vertikalen Bytes des LCD's
 boardSize			equ			336				; Byteanzahl des Buffers
 byteSize			equ			8				; Größe eines Bytes in Bit
-rowLength			equ			56				; Länge einer Zeile des Spielfeldes in Pixeln
-cursorRow			equ			6				; Vertikale Position des Cursors in Byte
-boardOffset			equ			36				; Der horizontale Versatz des Spielfeldes
-boardMiddleColumn	equ			24				; Die mittlere Spalte des Spielfeldes
+rowLength			equ			56				; Zeilenlänge des Spielfeldes
+												; in Pixeln
+
+cursorRow			equ			6				; Vertikale Position des Cursors
+												; in Byte
+
+boardOffset			equ			36				; horizontaler Versatz des 
+												; Spielfeldes
+
+boardMiddleColumn	equ			24				; Die mittlere Spalte des
+												; Spielfeldes
 
 
 ;-------------------------------------------------------------------------------
@@ -86,20 +93,20 @@ boardMiddleColumn	equ			24				; Die mittlere Spalte des Spielfeldes
 ;-------------------------------------------------------------------------------
 
 turnText
-		dc.b 		21,$02,$7E,$02,$00	; T
-		dc.b 		$3C,$40,$40,$7C,$00	; u
-		dc.b 		$7C,$08,$04,$08,$00	; r
-		dc.b 		$7C,$08,$04,$78,$00	; n
-		dc.b 		$28,$00				; :
+		dc.b 	21,$02,$7E,$02,$00		; T
+		dc.b 	$3C,$40,$40,$7C,$00		; u
+		dc.b 	$7C,$08,$04,$08,$00		; r
+		dc.b 	$7C,$08,$04,$78,$00		; n
+		dc.b 	$28,$00					; :
         
 playerText
-		dc.b 		30,$7E,$12,$12,$0C,$00 	; P
-		dc.b $02,$7E,$00				; l
-		dc.b $20,$54,$54,$78,$00		; a
-		dc.b $1C,$A0,$A0,$7C,$00		; y
-		dc.b $38,$54,$54,$48,$00		; e
-		dc.b $7C,$08,$04,$08,$00		; r
-		dc.b $00,$00					; 
+		dc.b 	30,$7E,$12,$12,$0C,$00 	; P
+		dc.b 	$02,$7E,$00				; l
+		dc.b 	$20,$54,$54,$78,$00		; a
+		dc.b	$1C,$A0,$A0,$7C,$00		; y
+		dc.b 	$38,$54,$54,$48,$00		; e
+		dc.b 	$7C,$08,$04,$08,$00		; r
+		dc.b 	$00,$00					; 
 				
 oneText	
 		dc.b 4,$04,$7E,$00,$00			; 1
@@ -109,9 +116,9 @@ twoText
 
 wonText	
 		dc.b 17,$00,$00,$3C,$40,$20
-		,$40,$3C,$00	; w
-		dc.b $38,$44,$44,$38,$00				; o
-		dc.b $7C,$08,$04,$78,$00				; n
+		,$40,$3C,$00					; w
+		dc.b $38,$44,$44,$38,$00		; o
+		dc.b $7C,$08,$04,$78,$00		; n
 
 
 ;-------------------------------------------------------------------------------
@@ -122,38 +129,39 @@ initGame
 
 		psha
 		
-		ldaa		#$0C				; Löscht das Terminal
-		jsr 		putChar
-		jsr			clrRam				; Überschreibt den Ram mit Nullen
-		jsr			initSPI				; Initialisierung der SPI-Schnittstelle
-		jsr			initLCD				; Initialisierung des Displays
-		jsr			LCDClr				; Löscht das Display
-		jsr			createGrid			; Schreibt das Spielfeld in den Buffer
-		jsr			drawBoard			; Schreibt den Bufferinhalt auf den Display
-		jsr			resetCursor			; Setz den Cursor in die Mitte des Spielfeldes
-		ldaa		#1
-		staa		player				; wechselt zu Spieler 1
-		jsr			showText
-		jsr			drawPlayer
+		ldaa	#$0C					; Löscht das Terminal
+		jsr 	putChar
+		jsr		clrRam					; Überschreibt den Ram mit Nullen
+		jsr		initSPI					; Initialisierung der SPI-Schnittstelle
+		jsr		initLCD					; Initialisierung des Displays
+		jsr		LCDClr					; Löscht das Display
+		jsr		createGrid				; Schreibt das Spielfeld in den Buffer
+		jsr		drawBoard				; Schreibt den Bufferinhalt auf den LCDs
+		jsr		resetCursor				; Setz den Cursor in die Mitte des
+										; Spielfeldes
+		ldaa	#1	
+		staa	player					; wechselt zu Spieler 1
+		jsr		showText
+		jsr		drawPlayer
 		pula
 
 		rts
 
 Start	
-		lds			#$3FFF				; Einstiegspunkt des Spieles
-		jsr			initGame			
-				
+		lds		#$3FFF					; Einstiegspunkt des Spieles
+		jsr		initGame			
+			
 mainLoop								; Hauptschleife
-		jsr			readInput
-		bra			mainLoop
-		
-End		bra			*
+		jsr		readInput
+		bra		mainLoop
+	
+End		bra		*
 
 
 ;-------------------------------------------------------------------------------
 ;   Vektortabelle
 ;-------------------------------------------------------------------------------
-VSection			Section
-		org			VRESET		; Reset-Vektor
-		dc.w		Start		; Programm-Startadresse
+VSection		Section
+		org		VRESET					; Reset-Vektor
+		dc.w	Start					; Programm-Startadresse
 
